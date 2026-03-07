a2113a1e2f785ec59019d3d2f4b5519ee122fb3f
Jan Vojtěch <jankovojtech@gmail.com>
2026-03-07 10:33:40 +0100

    Limit rating input to 1-5

--- js/qr.js
+++
@@ -1,6 +0,0 @@
-export async function renderQr(container, url, size = 220) {
-  container.innerHTML = "";
-  const canvas = document.createElement("canvas");
-  await QRCode.toCanvas(canvas, url, { width: size, margin: 2 });
-  container.appendChild(canvas);
-}
